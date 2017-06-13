d7f333cf9306abe36d14bd850fcbf26fb18f03e0
Kelly Whiting <kwhiting@gmail.com>
2017-06-13 11:24:44 -0700

    extend updated

--- src/underbar.js
+++ src/underbar.js
@@ -251,7 +251,13 @@
   //   }, {
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
-  _.extend = function(obj) {
+  _.extend = function(obj, source) {
+    for (var i = 1; i < arguments.length; i++){
+      _.each(arguments[i], function(item, index){
+        obj[index] = item;
+      });
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
